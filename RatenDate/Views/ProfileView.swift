60650928783cbdb789384c72f9799fc93a0b56be
Shivam <shivamdpat94@gmail.com>
2024-01-02 20:12:37 -0500

    Adding an initial ProvileView

---
+++ RatenDate/Views/ProfileView.swift
@@ -0,0 +1,18 @@
+//
+//  ProfileView.swift
+//  RatenDate
+//
+//  Created by Shivam Patel on 1/2/24.
+//
+
+import SwiftUI
+
+struct ProfileView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ProfileView()
+}
