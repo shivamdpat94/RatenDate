cd60f2f59e97f605694b553f4da6fcf163681e0d
Shivam <shivamdpat94@gmail.com>
2024-01-09 16:43:39 -0500

    Displaying all profile attributes in profileview.

--- RatenDate/Views/MainTabView.swift
+++ RatenDate/Views/MainTabView.swift
@@ -3,47 +3,48 @@ import SwiftUI
 struct MainTabView: View {
     @Environment(\.colorScheme) var colorScheme
     @State private var isLoginSuccessful: Bool = false
+    @State private var selectedTab: Int = 0 // Track the selected tab
 
     var body: some View {
         VStack(spacing: 0) {
-            // Banner at the top
             Text("LemonLime")
                 .font(Font.custom("Nunito-Black", size: 20))
 
-//                .padding() // Add some padding at the top for spacing
-//                .foregroundColor(colorScheme == .dark ? .white : .black)
-
-
-            TabView {
+            TabView(selection: $selectedTab) {
                 ProfileStackView()
                     .tabItem {
-                        tabIcon("lemon favicon-32x32")
+                        tabIcon("lemon favicon-32x32", isSelected: selectedTab == 0)
                         Text("Profiles")
                     }
-                
+                    .tag(0)
+
                 RateView()
                     .tabItem {
-                        tabIcon("rate favicon-32x32")
+                        tabIcon("rate favicon-32x32", isSelected: selectedTab == 1)
                         Text("Ratings")
                     }
-                
+                    .tag(1)
+
                 LikesMeView()
                     .tabItem {
-                        tabIcon("hearts favicon-32x32")
+                        tabIcon("hearts favicon-32x32", isSelected: selectedTab == 2)
                         Text("LikesMe")
                     }
-                
+                    .tag(2)
+
                 MatchView()
                     .tabItem {
-                        tabIcon("chat favicon-32x32")
+                        tabIcon("chat favicon-32x32", isSelected: selectedTab == 3)
                         Text("Match")
                     }
-                
+                    .tag(3)
+
                 SettingsView()
                     .tabItem {
-                        tabIcon("profile favicon-32x32")
+                        tabIcon("profile favicon-32x32", isSelected: selectedTab == 4)
                         Text("Profile")
                     }
+                    .tag(4)
             }
             .background(Color.white)
             .navigationBarBackButtonHidden(true)
@@ -54,29 +55,26 @@ struct MainTabView: View {
             }
         }
         .background(
-            Image("lemonfinal") // Reference your image asset here
-                .resizable() // Make the image resizable
-                .scaledToFill() // Scale the image to fill the background
-                .edgesIgnoringSafeArea(.all) // Ignore the safe area to cover the whole screen
-                .opacity(0.25) // Make the image 25% transparent
+            Image("lemonfinal")
+                .resizable()
+                .scaledToFill()
+                .opacity(0.25)
+                .edgesIgnoringSafeArea(.all)
                 .padding(.top, 22.9)
-
         )
     }
 
-    // Helper function to create tab icons
-    private func tabIcon(_ imageName: String) -> some View {
-        Image(imageName)
+    private func tabIcon(_ imageName: String, isSelected: Bool) -> some View {
+        Image(isSelected ? "green \(imageName)" : imageName) // Choose the green version if selected
             .resizable()
             .aspectRatio(contentMode: .fit)
-            .frame(width: 30, height: 30) // Adjusted size to a reasonable value
+            .frame(width: 30, height: 30)
             .if(colorScheme == .dark) {
-                $0.colorInvert() // Invert colors if in dark mode
+                $0.colorInvert()
             }
     }
 }
 
-// Extension to conditionally apply modifiers
 extension View {
     @ViewBuilder func `if`<Transform: View>(_ condition: Bool, transform: (Self) -> Transform) -> some View {
         if condition {
--- RatenDate/Views/ProfileView.swift
+++ RatenDate/Views/ProfileView.swift
@@ -11,16 +11,36 @@ struct ProfileView: View {
     var profile: RetrievedProfile
 
     var body: some View {
-        // Construct your view for each profile
-        VStack {
-            Text(profile.firstName)
-            // Add more UI elements as needed
+        ScrollView {
+            VStack(alignment: .leading, spacing: 10) {
+                ForEach(profile.photoURLs, id: \.self) { photoURL in
+                    AsyncImage(url: URL(string: photoURL)) { image in
+                        image.resizable()
+                             .aspectRatio(contentMode: .fill) // Fill the width of the screen
+                    } placeholder: {
+                        ProgressView()
+                    }
+                    .frame(width: UIScreen.main.bounds.width) // Set the width to the screen's width
+                    .clipped() // Clip the overflowing part
+                }
+
+                // Display other profile attributes
+                Text("Name: \(profile.firstName)")
+                Text("Age: \(profile.age)")
+                Text("Bio: \(profile.bio)")
+                Text("Ethnicity: \(profile.ethnicity)")
+                Text("Gender: \(profile.gender)")
+                Text("Interests: \(profile.interests.joined(separator: ", "))")
+                Text("Looking For: \(profile.lookingFor)")
+                // Add more attributes as needed
+            }
+            .padding()
         }
-        .frame(maxWidth: .infinity, maxHeight: .infinity) // Expand the VStack to fill the screen
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
     }
 }
 
-// PreviewProvider for ProfileView
+
 struct ProfileView_Previews: PreviewProvider {
     static var previews: some View {
         // Create a sample profile to display in the preview
