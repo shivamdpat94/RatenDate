9c2e4385de458aaf55d7bca09e8983b4a2653f94
Shivam <shivamdpat94@gmail.com>
2024-01-20 12:52:04 -0500

    Reverting main and manually adding chat view minus AWS

---
+++ RatenDate/Models/MessageFB.swift
@@ -0,0 +1,34 @@
+//
+//  Message.swift
+//  RatenDate
+//
+//  Created by Shivam Patel on 1/18/24.
+//
+
+import Foundation
+import FirebaseFirestore // Import Firestore to use Timestamp
+
+struct MessageFB: Codable {
+    var text: String
+    var timestamp: Timestamp
+    var firstName: String
+    var email: String
+
+    // Initialize the struct with all properties
+    init(text: String, timestamp: Timestamp = Timestamp(date: Date()), firstName: String, email: String) {
+        self.text = text
+        self.timestamp = timestamp
+        self.firstName = firstName
+        self.email = email
+    }
+
+    // Helper to convert to a dictionary, useful for uploading to Firestore
+    var dictionary: [String: Any] {
+        return [
+            "text": text,
+            "timestamp": timestamp,
+            "firstName": firstName,
+            "email": email
+        ]
+    }
+}
---
+++ RatenDate/Views/ChatBubbleView.swift
@@ -0,0 +1,39 @@
+//
+//  ChatBubbleView.swift
+//  RatenDate
+//
+//  Created by Mitchell Buff on 1/13/24.
+//
+
+import SwiftUI
+
+struct ChatBubbleView: View {
+    let message: Message
+    
+    var body: some View {
+        // Define custom colors using the hexadecimal values
+        let customGreen = Color(red: 166 / 255, green: 242 / 255, blue: 108 / 255)
+        let customGray = Color(red: 217 / 255, green: 217 / 255, blue: 217 / 255)
+        
+        HStack {
+            if message.isCurrentUser {
+                Spacer()
+            }
+            
+            Text(message.text)
+                .padding(8)
+                .foregroundColor(.black) // Text color is now black for all messages
+                .background(message.isCurrentUser ? customGreen : customGray)
+                .cornerRadius(12)
+                .shadow(color: .gray, radius: 3, x: 0, y: 3) // Shadow with offset
+            
+            if !message.isCurrentUser {
+                Spacer()
+            }
+        }
+        .padding(message.isCurrentUser ? .leading : .trailing, 60)
+        .padding(.horizontal, 20)
+        .transition(.slide)
+        .animation(.default)
+    }
+}
