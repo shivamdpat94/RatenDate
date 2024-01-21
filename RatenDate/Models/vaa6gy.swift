5396607b8caaad9ee93ed83cde08959317c172b4
Shivam <shivamdpat94@gmail.com>
2024-01-20 22:30:49 -0500

    Deleting test files

--- RatenDate/Models/vaa6gy.swift
+++
@@ -1,8 +0,0 @@
-//
-//  vaa6gy.swift
-//  RatenDate
-//
-//  Created by Mitchell Buff on 1/20/24.
-//
-
-import Foundation
--- RatenDate/Models/vabudf.swift
+++
@@ -1,8 +0,0 @@
-//
-//  vabudf.swift
-//  RatenDate
-//
-//  Created by Shivam Patel on 1/20/24.
-//
-
-import Foundation
