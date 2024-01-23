f793a6efb14e3570d486de858e598eb3002c52f9
Tom Buff <credcloudapp@gmail.com>
2024-01-22 20:09:56 -0500

    Push notificaiton update

--- RatenDate/RatenDateApp.swift
+++ RatenDate/RatenDateApp.swift
@@ -1,16 +1,36 @@
 import SwiftUI
 import Firebase
 import UserNotifications
+import FirebaseMessaging
 
 // Define a custom AppDelegate
-class CustomAppDelegate: UIResponder, UIApplicationDelegate {
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+class CustomAppDelegate: UIResponder, UIApplicationDelegate, MessagingDelegate {
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]? = nil) -> Bool {
+        // Configure Firebase
+        FirebaseApp.configure()
+
         // Request notification permission
         UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .badge, .sound]) { granted, error in
             // Handle the granted permission or error
         }
         application.registerForRemoteNotifications() // Register for remote notifications
+
+        // Set Messaging delegate
+        Messaging.messaging().delegate = self
+
         return true
+    }
+
+    func application(_ application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
+        // Here you can send the deviceToken to Firebase or your server if needed
+    }
+
+    func application(_ application: UIApplication, didFailToRegisterForRemoteNotificationsWithError error: Error) {
+        print("FAILED TO REGISTER FOR REMOTE NOTIFCATIONS!")
+    }
+
+    func messaging(_ messaging: Messaging, didReceiveRegistrationToken fcmToken: String?) {
+        print("GOT THE FCM TOKEN!")
     }
 
     // Add other necessary AppDelegate methods if needed
@@ -21,10 +41,6 @@ struct RatenDateApp: App {
     @UIApplicationDelegateAdaptor(CustomAppDelegate.self) var appDelegate
     @StateObject var sessionManager = UserSessionManager()
     @State var isShowingLoadingScreen = true
-
-    init() {
-        FirebaseApp.configure()
-    }
 
     var body: some Scene {
         WindowGroup {
